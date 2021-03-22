7c10f1d41d273a06393660ba65e9673fb63e1e5b
ImaneMF01 <imane.mniefilali@bbc.co.uk>
2021-03-22 08:43:41 +0000

    index1 with Bitmovin config

---
+++ bitmovinp50.js
@@ -0,0 +1,129 @@
+    {
+        //////////////////// hev1.2.4.L153.00
+
+        ///////////////Bitmovin p50///////////////
+        ////////////////////////////////////////
+
+    type: 'media-source',
+        video: {
+            contentType: 'video/mp4; codecs=hev1.2.4.L153.00',
+            width: 620,
+            height: 360,
+            bitrate: 350,
+            framerate: 50,
+            colorGamut: 'rec2020',
+            transferFunction: 'hlg'
+
+        }
+
+    },
+
+    {
+    type: 'media-source',
+        video: {
+            contentType: 'video/mp4; codecs=hev1.2.4.L153.00',
+            width: 768,
+            height: 432,
+            bitrate: 550,
+            framerate: 50
+
+        }
+
+    },    
+
+    {
+        /// 960 540 p   50  1   1000    CBR HEVC    Main 10 5.1
+    type: 'media-source',
+        video: {
+            contentType: 'video/mp4; codecs=hev1.2.4.L153.00',
+            width: 960,
+            height: 540,
+            bitrate: 1000,
+            framerate: 50
+
+        }
+
+    },  
+
+    {
+    ////    1280    720 p   50  1.6 1600    CBR HEVC    Main 10 5.1
+    type: 'media-source',
+        video: {
+            contentType: 'video/mp4; codecs=hev1.2.4.L153.00',
+            width: 1280,
+            height: 720,
+            bitrate: 1600,
+            framerate: 50
+
+        }
+
+    },  
+
+    {
+        ///     1600    900 p   50  2.6 2600    CBR HEVC    Main 10 5.1
+    type: 'media-source',
+        video: {
+            contentType: 'video/mp4; codecs=hev1.2.4.L153.00',
+            width: 1600,
+            height: 900,
+            bitrate: 2600,
+            framerate: 50
+
+        }
+
+    },  
+{
+    ////    1920    1080    p   50  4.6 4600    CBR HEVC    Main 10 5.1
+    type: 'media-source',
+        video: {
+            contentType: 'video/mp4; codecs=hev1.2.4.L153.00',
+            width: 1920,
+            height: 1080,
+            bitrate: 4600,
+            framerate: 50
+
+        }
+
+    },  
+{
+    ////    2560    1440    p   50  8.5 8500    CBR HEVC    Main 10 5.1
+    type: 'media-source',
+        video: {
+            contentType: 'video/mp4; codecs=hev1.2.4.L153.00',
+            width: 2560,
+            height: 1440,
+            bitrate: 8500,
+            framerate: 50
+
+        }
+
+    }, 
+{
+    ////    3200    1800    p   50  15  15000   CBR HEVC    Main 10 5.1
+    type: 'media-source',
+        video: {
+            contentType: 'video/mp4; codecs=hev1.2.4.L153.00',
+            width: 3200,
+            height: 1800,
+            bitrate: 15000,
+            framerate: 50
+
+        }
+
+    },  
+{
+    /////   3840    2160    p   50  25  25000   CBR HEVC    Main 10 5.1
+    type: 'media-source',
+        video: {
+            contentType: 'video/mp4; codecs=hev1.2.4.L153.00',
+            width: 3840,
+            height: 2160,
+            bitrate: 25000,
+            framerate: 50
+
+        }
+
+    },  
+
+    //////////////////// hev1.2.4.L153.00
+    //////////////////////////////////////////////////
